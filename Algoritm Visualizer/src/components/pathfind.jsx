e521bc900d4644eae0fe0b5f3c3de91880385fa5
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-07-17 19:51:11 +0530

    Add files via upload

---
+++ Algoritm Visualizer/src/components/pathfind.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Pathfind extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pathfind Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Pathfind;
